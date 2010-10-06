9e47df25b103f31b14f6de3698cd4c98898c6859
Caleb Deveraux <carrus85@gmail.com>
2010-10-06 11:54:58 +0800

    Don't throw in mce.py on fill/replace with an empty box
    mce.py, when called with an empty box (e.g., request entire world to be
    changed), would throw an exception, since box is None (and therefore has
    no volume parameter).  This would prevent the ligthting steps from
    occuring further on.

--- mce.py
+++ mce.py
@@ -264,7 +264,7 @@ class mce(object):
         
         
         self.needsSave = True;
-        print "Filled {0} blocks.".format(box.volume) 
+        print "Filled {0} blocks.".format("all" if box is None else box.volume)
     
     def _replace(self, command):
         """
@@ -302,8 +302,8 @@ class mce(object):
         self.level.fillBlocks(box, newBlockType, blockData = 0, blocksToReplace = [blockType])
         
         self.needsSave = True;
-        print "Replaced {0} blocks.".format(box.volume) 
-    
+        print "Replaced {0} blocks.".format("all" if box is None else box.volume)
+
     def _analyze(self, command):
         """
     analyze
