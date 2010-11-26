8f1724b0db48a2644ca861f963659054e3947677
David Vierra <codewarrior@hawaii.rr.com>
2010-11-26 03:42:47 -1000

    need to make allChunks into a list before putting it into ndarray

--- mclevel.py
+++ mclevel.py
@@ -1814,7 +1814,7 @@ class MCInfdevOldLevel(MCLevel):
         if len(self.allChunks) == 0:
             return BoundingBox( (0,0,0), (0,0,0) )
             
-        allChunksArray = array(self.allChunks)
+        allChunksArray = array(list(self.allChunks), dtype='uint32')
         mincx = min(allChunksArray[:,0])
         maxcx = max(allChunksArray[:,0])
         mincz = min(allChunksArray[:,1])
