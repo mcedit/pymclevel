25e4ec07c12891e8708cca6613b47f468ddb78c7
David Vierra <codewarrior@hawaii.rr.com>
2011-02-07 18:55:54 -1000

    update regression test for new block names

--- run_regression_test.py
+++ run_regression_test.py
@@ -150,8 +150,8 @@ alpha_tests = [
     (do_test,               'degrief',  '403e6c6147cf1f8d73377b18bbf5e4973606a311', ['degrief']),
     (do_test_match_output,  'analyze',  'fabcae21e6d581bb00d11f1a59dbd503b15c0de8', ['analyze']),
     (do_test,               'relight',  'e0cf60c62adfdb313f198af5314c31f89d158c12', ['relight']),
-    (do_test,               'replace',  'd73767293e903b6d1c49c1838eb1849b69d83ad8', ['replace', 'Water', 'with', 'Lava']),
-    (do_test,               'fill',     'f4f57c3d902b6894031d416cb9279232e7e24bd7', ['fill', 'Water']),
+    (do_test,               'replace',  'd73767293e903b6d1c49c1838eb1849b69d83ad8', ['replace', 'Water (active)', 'with', 'Lava (active)']),
+    (do_test,               'fill',     'f4f57c3d902b6894031d416cb9279232e7e24bd7', ['fill', 'Water (active)']),
     (do_test,               'heightmap',     '9e7460d39c8e0456789cf89fee45276db2719aaa', ['heightmap', 'regression_test/mars.png']),
 ]
 
