34a8e1bd21b10674099c0799b8a8df30675b46cd
David Vierra <codewarrior@hawaii.rr.com>
2011-04-08 12:36:05 -1000

    fix any offsets pointing outside of the file

--- mclevel.py
+++ mclevel.py
@@ -2185,7 +2185,9 @@ class MCRegionFile(object):
                 f.truncate(filesize)
             
             if filesize == 0:
-                f.truncate(self.SECTOR_BYTES*2)
+                filesize = self.SECTOR_BYTES*2
+                f.truncate(filesize)
+                
             
             f.seek(0)
             offsetsData = f.read(self.SECTOR_BYTES)
@@ -2204,7 +2206,11 @@ class MCRegionFile(object):
             count = offset & 0xff
             
             for i in xrange(sector, sector+count):
-                if i >= len(self.freeSectors): raise RegionMalformed, "Region file offset table points to sector {0} (past the end of the file)".format(i)
+                if i >= len(self.freeSectors): 
+                    #raise RegionMalformed, "Region file offset table points to sector {0} (past the end of the file)".format(i)
+                    print  "Region file offset table points to sector {0} (past the end of the file)".format(i)
+                    needsRepair = True
+                    break
                 if self.freeSectors[i] is False:
                     needsRepair = True
                 self.freeSectors[i] = False
@@ -2231,8 +2237,12 @@ class MCRegionFile(object):
                 cz += rz << 5
                 sectorStart = offset >> 8
                 sectorCount = offset & 0xff
-                
                 try:
+                
+                    if sectorStart + sectorCount > len(self.freeSectors):
+                        raise RegionMalformed,  "Offset {start}:{end} ({offset}) at index {index} pointed outside of the file".format(
+                            start=sectorStart, end=sectorStart+sectorCount, index=index, offset=offset)
+                        
                     compressedData = self._readChunk(cx,cz)
                     if compressedData is None: 
                         raise RegionMalformed, "Failed to read chunk data for {0}".format((cx,cz))
