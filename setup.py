b1f473af07147305be19d24c2c4a16496b7c9455
David Vierra <codewarrior@hawaii.rr.com>
2011-09-15 23:28:47 -1000

    Added distutils setup script written by user:sixohsix.
    changed setup arguments, adding an explicit packages list and a package_dir override that allows me to keep the current folder structure. tests.py and run_regression_test.py are included in the dist as a result, and egg-info/SOURCES.txt is slightly mangled but the docs say it's not used for anything anyway.
    
    plus, `setup.py develop` still works.

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.0'
+
+install_requires = [
+    # -*- Extra requirements: -*-
+    "numpy",
+    ]
+
+setup(name='pymclevel',
+      version=version,
+      description="Python library for reading Minecraft levels",
+      long_description=open("./README.txt", "r").read(),
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Console",
+          "Intended Audience :: End Users/Desktop",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: MIT License",
+          ],
+      keywords='minecraft',
+      author='David Vierra',
+      author_email='codewarrior0@gmail.com',
+      url='https://github.com/codewarrior0/pymclevel',
+      license='MIT License',
+      package_dir={'pymclevel':'.'},
+      packages=["pymclevel"],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      entry_points="""
+      # -*- Entry points: -*-
+      [console_scripts]
+      mce.py=pymclevel.mce:main
+      """,
+      )
