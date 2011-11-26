e71e1594612134a282bf77c0415776f63b74fd4d
David Vierra <codewarrior@hawaii.rr.com>
2011-11-26 01:32:04 -1000

    add float variant for bounding box

--- box.py
+++ box.py
@@ -1,13 +1,13 @@
 import itertools
 
 class BoundingBox (object):
-
+    type = int
     def __init__(self, origin=(0, 0, 0), size=(0, 0, 0)):
         if isinstance(origin, BoundingBox):
             self._origin = list(origin._origin)
             self._size = list(origin._size)
         else:
-            self._origin, self._size = list(map(int, origin)), list(map(int, size))
+            self._origin, self._size = list(map(self.type, origin)), list(map(self.type, size))
 
     def chunkBox(self, level):
         """Returns this box extended to the chunk boundaries of the given level"""
@@ -87,9 +87,9 @@ class BoundingBox (object):
     def getHeight(self): return self._size[1];
     def getLength(self): return self._size[2];
 
-    def setWidth(self, w): self.size[0] = max(0, int(w))
-    def setHeight(self, h): self.size[1] = max(0, int(h))
-    def setLength(self, l): self.size[2] = max(0, int(l))
+    def setWidth(self, w): self.size[0] = max(0, self.type(w))
+    def setHeight(self, h): self.size[1] = max(0, self.type(h))
+    def setLength(self, l): self.size[2] = max(0, self.type(l))
 
     width = property(getWidth, setWidth, None, "The dimension along the X axis");
     height = property(getHeight, setHeight, None, "The dimension along the Y axis");
@@ -175,3 +175,6 @@ class BoundingBox (object):
 
     def __repr__(self):
         return "BoundingBox({0}, {1})".format(self.origin, self.size)
+        
+class FloatBox (BoundingBox):
+    type = float
