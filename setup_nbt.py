5687de8f3687b92e4a1f812921a3630882c8a0f7
David Vierra <codewarrior@hawaii.rr.com>
2011-12-27 17:09:47 -1000

    Tell distutils to add numpy's include dir to the list of include paths.

--- setup_nbt.py
+++ setup_nbt.py
@@ -4,9 +4,11 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 ext_modules = [Extension("_nbt", ["_nbt.pyx"])]
+import numpy
 
 setup(
   name = 'NBT library (Cython implementation)',
   cmdclass = {'build_ext': build_ext},
-  ext_modules = ext_modules
+  ext_modules = ext_modules,
+  include_dirs = numpy.get_include()
 )
