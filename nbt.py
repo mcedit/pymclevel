167f7e960cc4891ba2d6c49eaab927c110c295eb
David Vierra <codewarrior@hawaii.rr.com>
2012-01-03 12:05:24 -1000

    Pass numpy's include dir to pyximport.install() to fix "error: numpy/arrayobject.h: No such file or directory".

--- nbt.py
+++ nbt.py
@@ -5,7 +5,8 @@ try:
         from _nbt import *
     except ImportError:
         print "Import error loading precompiled _nbt extension. Trying pyximport..."
-        from pyximport import install; install()
+        import numpy
+        from pyximport import install; install(setup_args={'include_dirs':[numpy.get_include()]})
         import _nbt
         from _nbt import *
 except ImportError, e:
