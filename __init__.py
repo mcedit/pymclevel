0f9997814bdf3d68e68e0197f7d7d1146ceeff91
David Sowder <github.com@david.sowder.com>
2012-03-04 18:53:27 -0600

    hack __init__.py to present the items we believe were previously available through the import alls we used to have scattered so much more widely across the package

--- __init__.py
+++ __init__.py
@@ -1,10 +1,15 @@
-from mclevel import fromFile, loadWorld, loadWorldNumber
-from mclevelbase import saveFileDir, minecraftDir
+from box import BoundingBox, FloatBox
+from entity import Entity, TileEntity
+from faces import *
 from indev import *
 from infiniteworld import *
+import items
 from java import *
 from level import *
-from schematic import *
 from materials import *
-
-import items
+from mclevelbase import saveFileDir, minecraftDir
+from mclevel import fromFile, loadWorld, loadWorldNumber
+from nbt import *
+from pocket import pocketMaterials
+import pocket
+from schematic import *
