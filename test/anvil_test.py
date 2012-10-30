5b5f3999cbc1c98e69e1d4449a8b99fd70d1c64b
David Vierra <codewarrior@hawaii.rr.com>
2012-10-29 16:37:54 -1000

    Test: Move the extended HeightMap test from mcr_test to anvil_test and give it a better name.
    This test only checks that the TAG_Int_Array-type HeightMap's byte order gets swapped correctly when saving and loading.

--- test/anvil_test.py
+++ test/anvil_test.py
@@ -1,10 +1,12 @@
 import itertools
+import os
 import shutil
 import unittest
 import numpy
 
 import mclevel
 from infiniteworld import MCInfdevOldLevel
+import nbt
 from schematic import MCSchematic
 from box import BoundingBox
 from templevel import mktemp, TempLevel
@@ -127,3 +129,21 @@ class TestAnvilLevel(unittest.TestCase):
         level.setPlayerSpawnPosition((0, 64, 0), "Player")
         level.getPlayerPosition()
         assert len(level.players) != 0
+
+    def testBigEndianIntHeightMap(self):
+        """ Test modifying, saving, and loading the new TAG_Int_Array heightmap
+        added with the Anvil format.
+        """
+        chunk = nbt.load("testfiles/AnvilChunk.dat")
+
+        hm = chunk["Level"]["HeightMap"]
+        hm.value[2] = 500
+        oldhm = numpy.array(hm.value)
+
+        filename = mktemp("ChangedChunk")
+        chunk.save(filename)
+        changedChunk = nbt.load(filename)
+        os.unlink(filename)
+
+        eq = (changedChunk["Level"]["HeightMap"].value == oldhm)
+        assert eq.all()
--- test/mcr_test.py
+++ test/mcr_test.py
@@ -1,8 +1,5 @@
-import os
-import numpy
-import nbt
 from anvil_test import TestAnvilLevel
-from templevel import TempLevel, mktemp
+from templevel import TempLevel
 
 __author__ = 'Rio'
 
@@ -11,20 +8,3 @@ class TestMCR(TestAnvilLevel):
         self.indevLevel = TempLevel("hell.mclevel")
         self.anvilLevel = TempLevel("PyTestWorld")
 
-    def testAnvilChunk(self):
-        """ Test modifying, saving, and loading the new TAG_Int_Array heightmap
-        added with the Anvil format.
-        """
-        chunk = nbt.load("testfiles/AnvilChunk.dat")
-
-        hm = chunk["Level"]["HeightMap"]
-        hm.value[2] = 500
-        oldhm = numpy.array(hm.value)
-
-        filename = mktemp("ChangedChunk")
-        chunk.save(filename)
-        changedChunk = nbt.load(filename)
-        os.unlink(filename)
-
-        eq = (changedChunk["Level"]["HeightMap"].value == oldhm)
-        assert eq.all()
