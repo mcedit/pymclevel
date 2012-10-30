adb6b7bb45f7bfb49e05fe083c44fc2ed8a1b00c
David Vierra <codewarrior@hawaii.rr.com>
2012-10-29 16:14:30 -1000

    Test: Place temporary copies of testing files into their own subfolder.
    Make sure temporary copies are removed on exit using atexit.register.

--- test/templevel.py
+++ test/templevel.py
@@ -1,3 +1,4 @@
+import atexit
 import os
 from os.path import join
 import shutil
@@ -6,8 +7,12 @@ import mclevel
 
 __author__ = 'Rio'
 
+tempdir = os.path.join(tempfile.gettempdir(), "pymclevel_test")
+if not os.path.exists(tempdir):
+    os.mkdir(tempdir)
+
 def mktemp(suffix):
-    td = tempfile.mkdtemp(suffix)
+    td = tempfile.mkdtemp(suffix, dir=tempdir)
     os.rmdir(td)
     return td
 
@@ -29,11 +34,17 @@ class TempLevel(object):
 
         self.tmpname = tmpname
         self.level = mclevel.fromFile(tmpname)
+        atexit.register(self.removeTemp)
 
     def __del__(self):
         if hasattr(self, 'level'):
             self.level.close()
             del self.level
+
+        self.removeTemp()
+
+    def removeTemp(self):
+
         if hasattr(self, 'tmpname'):
             filename = self.tmpname
 
