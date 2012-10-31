ea585d7627c784a1fca99c5673954721d2ab5c07
David Vierra <codewarrior@hawaii.rr.com>
2012-10-30 17:57:24 -1000

    Test: test_mcr no longer re-runs TestAnvilLevel.

--- test/mcr_test.py
+++ test/mcr_test.py
@@ -1,9 +1,9 @@
-from anvil_test import TestAnvilLevel
+import anvil_test
 from templevel import TempLevel
 
 __author__ = 'Rio'
 
-class TestMCR(TestAnvilLevel):
+class TestMCR(anvil_test.TestAnvilLevel):
     def setUp(self):
         self.indevLevel = TempLevel("hell.mclevel")
         self.anvilLevel = TempLevel("PyTestWorld")
